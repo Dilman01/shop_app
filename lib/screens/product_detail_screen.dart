2f294929e09a2931deb367b63d483e77c4b65aca
Dilman Arif <dilmanarif01@gmail.com>
2022-07-08 16:50:11 +0300

    Add Navigaiton to details screen

---
+++ lib/screens/product_detail_screen.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  // final String title;
+
+  // ProductDetailScreen(this.title);
+
+  static const routeName = 'Product-detail';
+
+  @override
+  Widget build(BuildContext context) {
+    final productId = ModalRoute.of(context)!.settings.arguments as String;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('title'),
+      ),
+    );
+  }
+}
