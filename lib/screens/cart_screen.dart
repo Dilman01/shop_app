9b82c45b2c0ccdb3ae881991585e08dd363f5b5f
Dilman Arif <dilmanarif01@gmail.com>
2022-07-09 15:48:37 +0300

    Add Orders

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/cart.dart' show Cart;
+import 'package:shop_app/providers/orders.dart';
 import '../widgets/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
@@ -44,7 +45,13 @@ class CartScreen extends StatelessWidget {
                   ),
                   TextButton(
                     child: Text('ORDER NOW'),
-                    onPressed: () {},
+                    onPressed: () {
+                      Provider.of<Orders>(context, listen: false).addOrders(
+                        cart.items.values.toList(),
+                        cart.totalAmount,
+                      );
+                      cart.clear();
+                    },
                     style: TextButton.styleFrom(
                       primary: Theme.of(context).primaryColor,
                     ),
