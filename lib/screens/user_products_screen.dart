9fee90f19a9e8237bc57d8c5d950c62c5a295e07
Dilman Arif <dilmanarif01@gmail.com>
2022-07-10 21:03:09 +0300

    Add Manage Products Screen

---
+++ lib/screens/user_products_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/widgets/app_drawer.dart';
+import 'package:shop_app/widgets/user_product_item.dart';
+
+import '../providers/products.dart';
+
+class UserProducsScreen extends StatelessWidget {
+  static const routeName = '/user-products';
+
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Products'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      drawer: AppDrawer(),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: ListView.builder(
+          itemCount: productsData.itmes.length,
+          itemBuilder: (_, index) => Column(
+            children: [
+              UserProductItem(
+                productsData.itmes[index].title,
+                productsData.itmes[index].imageUrl,
+              ),
+              Divider(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
